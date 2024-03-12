c39ddd81f2233bc4690fc68ce28f1c2d2d39c7b9
Jaquan Rhodes <jaquanrhodes@gmail.com>
2024-03-12 08:10:12 -0500

    final commit

---
+++ U2GP1Starting/server.js
@@ -0,0 +1,20 @@
+// Set the 'NODE_ENV' variable
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// Load the 'express' module
+const configureExpress = require('./config/express');
+const configureMongoose = require('./config/mongoose');
+
+const db = configureMongoose();
+
+// Create a new Express application instance
+const app = configureExpress();
+
+// Use the Express application instance to listen to the '3000' port
+app.listen(3000);
+
+// Log the server status to the console
+console.log('Server running at http://localhost:3000/');
+
+// Use the module.exports property to expose our Express application instance for external usage
+module.exports = app;
